37ee550387fa97113f6483307f2005e22367bfcc
Robin Ung <Robin_ung@hotmail.com>
2024-03-08 21:53:56 +0000

    Change migration and models

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
+
+# Create your models here.
 
 
 class Post(models.Model):
@@ -21,22 +22,20 @@ class Post(models.Model):
         ordering = ["-created_on"]
 
     def __str__(self):
-        return (f"{self.title} written by {self.author}")
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"
-    )
+        Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter"
-    )
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ["created_on"]
 
     def __str__(self):
-        return (f"{self.body} by {self.author}")
+        return f"Comment {self.body} by {self.author}"
