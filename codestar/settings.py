a2d22d1639b8dc3d5bd53d1ec65b6fb85fe30ddc
Robin Ung <Robin_ung@hotmail.com>
2024-03-10 19:08:01 +0000

    Add index and base html

--- codestar/settings.py
+++ codestar/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 
 STATIC_ROOT = ''
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
