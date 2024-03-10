e90141fa2e32d86794c8b6753322e2c26cccf227
Robin Ung <Robin_ung@hotmail.com>
2024-03-10 15:20:26 +0000

    Testing migrations

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ["-created_on", "author"]
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
@@ -27,12 +27,18 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments_author",
+    )
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ["created_on"]
